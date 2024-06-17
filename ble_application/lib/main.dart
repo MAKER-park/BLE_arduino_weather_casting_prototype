41fb2696470c9bb87eab0d16b0f813910f54d13c
Park jae wan <hanulcom7@naver.com>
2024-06-17 12:48:34 +0900

    ble serach and add
    add

--- ble_application/lib/main.dart
+++ ble_application/lib/main.dart
@@ -1,125 +1,146 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter BLE Communication',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+  BluetoothDevice? _device;
+  BluetoothCharacteristic? _characteristic;
+  List<BluetoothDevice> _devicesList = [];
+  bool _isScanning = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _requestPermissions().then((_) {
+      _scanForDevices();
     });
+  }
+
+  Future<void> _requestPermissions() async {
+    if (await Permission.bluetoothScan.request().isGranted &&
+        await Permission.bluetoothConnect.request().isGranted &&
+        await Permission.location.request().isGranted) {
+      // Permissions granted
+    }
+  }
+
+  void _scanForDevices() {
+    if (_isScanning) {
+      flutterBlue.stopScan();
+    }
+
+    setState(() {
+      _isScanning = true;
+    });
+
+    flutterBlue.startScan(timeout: Duration(seconds: 4)).catchError((error) {
+      print("Error starting scan: $error");
+    });
+
+    flutterBlue.scanResults.listen((results) {
+      setState(() {
+        _devicesList = results.map((result) => result.device).toList();
+        _isScanning = false;
+      });
+    });
+
+    flutterBlue.isScanning.listen((isScanning) {
+      setState(() {
+        _isScanning = isScanning;
+      });
+    });
+  }
+
+  void _connectToDevice(BluetoothDevice device) async {
+    await device.connect();
+    _device = device;
+
+    List<BluetoothService> services = await device.discoverServices();
+    services.forEach((service) {
+      if (service.uuid.toString() == "0000180c-0000-1000-8000-00805f9b34fb") {
+        service.characteristics.forEach((characteristic) {
+          if (characteristic.uuid.toString() == "00002a56-0000-1000-8000-00805f9b34fb") {
+            _characteristic = characteristic;
+          }
+        });
+      }
+    });
+
+    setState(() {}); // Update the UI after connection
+  }
+
+  void _sendMessage(bool message) async {
+    if (_characteristic != null) {
+      await _characteristic!.write([message ? 1 : 0]);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Flutter BLE Communication'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            ElevatedButton(
+              onPressed: _scanForDevices,
+              child: Text(_isScanning ? 'Scanning...' : 'Scan for Devices'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _devicesList.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_devicesList[index].name),
+                    subtitle: Text(_devicesList[index].id.toString()),
+                    onTap: () => _connectToDevice(_devicesList[index]),
+                  );
+                },
+              ),
             ),
+            SizedBox(height: 20),
+            if (_device != null) ...[
+              Text('Connected to ${_device!.name}'),
+              ElevatedButton(
+                onPressed: () => _sendMessage(true),
+                child: Text('Send ON Message'),
+              ),
+              ElevatedButton(
+                onPressed: () => _sendMessage(false),
+                child: Text('Send OFF Message'),
+              ),
+            ],
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
